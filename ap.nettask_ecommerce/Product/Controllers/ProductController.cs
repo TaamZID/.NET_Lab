e13017998d225708f86b73ca562ef91161e6d6fd
TaamZID <tamjidalam00@gmail.com>
2021-10-05 20:57:33 +0600

    ap.nettask_ecommerce

---
+++ ap.nettask_ecommerce/Product/Controllers/ProductController.cs
@@ -0,0 +1,106 @@
+﻿using Product.Models;
+using Product.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+namespace Product.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            Database db = new Database();
+            var products = db.Products.GetAll();
+            return View(products);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Productype p)
+        {
+            if (ModelState.IsValid)
+            {
+                Database db = new Database();
+                db.Products.Add(p);
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
+        public ActionResult AddToCart (int id)
+        {
+            Database db = new Database();
+            Productype p = db.Products.AddToCart(id);
+
+            Session["Id"] = p.Id;
+            Session["Name"] = p.Name;
+            Session["Price"] = p.Price;
+            Session["Quantity"] = p.Quantity;
+            Session["Description"] = p.Description;
+
+            if (p!=null)
+            {
+                List<Productype> products = null;
+                if (Session["Cart"] == null)
+                {
+                    products = new List<Productype>();
+                }
+                else
+                {
+                    string json2 = Session["Cart"].ToString();
+                    products = new JavaScriptSerializer().Deserialize<List<Productype>>(json2);
+                }
+                products.Add(p);
+                string json = new JavaScriptSerializer().Serialize(products);
+                Session["Cart"] = json;
+            }
+            return RedirectToAction("Cart");
+        }
+
+        public ActionResult Cart()
+        {
+            string json = Session["Cart"].ToString();
+            var products = new JavaScriptSerializer().Deserialize<List<Productype>>(json);
+            return View(products);
+        }
+
+        [HttpGet]
+        public ActionResult Orders()
+        {
+            Database db = new Database();
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Update(int id)
+        {
+            Database db = new Database();
+            var p = db.Products.Get(id);
+            return View(p);
+        }
+        [HttpPost]
+        public ActionResult Update(Productype product)
+        {
+            Database db = new Database();
+            db.Products.Update(product);
+            return RedirectToAction("Update");
+
+        }
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            Database db = new Database();
+            db.Products.Delete(id);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ ap.nettask_ecommerce/Product/Models/Tables/Products.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using Product.Models.Entity;
+
+namespace Product.Models.Tables
+{
+    public class Products
+    {
+        SqlConnection conn;
+        public Products(SqlConnection conn)
+        {
+            this.conn = conn;
+        }
+        public void Add(Productype p)
+        {
+            string query = String.Format("Insert into ProductList values ('{0}','{1}','{2}','{3}')", p.Name, p.Price, p.Quantity, p.Description);
+            SqlCommand cmd = new SqlCommand(query, conn);
+            conn.Open();
+            int r = cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+        public Productype Get(int id)
+        {
+            return null;
+        }
+        public List<Productype> GetAll()
+        {
+            string query = "select * from ProductList";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            conn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            List<Productype> products = new List<Productype>();
+            while (reader.Read())
+            {
+                Productype s = new Productype()
+                {
+                    Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                    Name = reader.GetString(reader.GetOrdinal("Name")),
+                    Price = reader.GetInt32(reader.GetOrdinal("Price")),
+                    Quantity = reader.GetInt32(reader.GetOrdinal("Quantity")),
+                    Description = reader.GetString(reader.GetOrdinal("Description"))
+                };
+                products.Add(s);
+            }
+            conn.Close();
+            return products;
+        }
+
+        public Productype AddToCart (int Id)
+        {
+            string query = String.Format("select * from ProductList where Id = {0}",Id);
+            SqlCommand cmd = new SqlCommand(query, conn);
+            conn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            Productype product= null;
+            while (reader.Read())
+            {
+                 product = new Productype()
+                 {
+                    Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                    Name = reader.GetString(reader.GetOrdinal("Name")),
+                    Price = reader.GetInt32(reader.GetOrdinal("Price")),
+                    Quantity = reader.GetInt32(reader.GetOrdinal("Quantity")),
+                    Description = reader.GetString(reader.GetOrdinal("Description"))
+                 };
+            }
+            conn.Close();
+            return product;
+        }
+        public void Update(Productype p)
+        {
+            string query = String.Format("UPDATE ProductList SET name='{0}', price={1}, quantity={2}, description='{3}' WHERE id={4}", p.Name, p.Price, p.Quantity, p.Description, p.Id);
+            SqlCommand cmd = new SqlCommand(query, conn);
+            conn.Open();
+            int r = cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+        public void Delete(int id)
+        {
+            string query = String.Format("DELETE FROM ProductList WHERE id={0}", id);
+            SqlCommand cmd = new SqlCommand(query, conn);
+            conn.Open();
+            int r = cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+    }
+}
